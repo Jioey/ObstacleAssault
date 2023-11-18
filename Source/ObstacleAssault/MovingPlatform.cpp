aec0cb7b4b9867425ca155b104ebe6cb60f76d74
Joey <55892851+JioeyKun@users.noreply.github.com>
2023-11-18 12:48:46 -0500

    Initial commit

---
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();	
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime) {
+	FVector EndLocation = StartLocation + (PlatformVelocity.GetSafeNormal() * MoveDist)
+	if (ShouldPlatformReturn(EndLocation)) {
+		FVector MoveDirection = ;
+		StartLocation = StartLocation + MoveDirection;
+		SetActorLocation(StartLocation);
+		PlatformVelocity = -PlatformVelocity;
+	} else {
+		FVector CurrentLocation = GetActorLocation();
+		CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+		SetActorLocation(CurrentLocation);
+	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime) {
+	FRotator CurrentRotation = GetActorRotation();
+	CurrentRotation = CurrentRotation + (RotationVelocity * DeltaTime);
+	SetActorRotation(CurrentRotation);
+}
+
+bool AMovingPlatform::ShouldPlatformReturn(FVector EndLocation) const {
+	return 
+	// GetDistMoved() >= MoveDist;
+}
+
+float AMovingPlatform::GetDistMoved() const {
+	return FVector::Dist(StartLocation, GetActorLocation());
+}
